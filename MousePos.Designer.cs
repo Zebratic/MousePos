f30f938d5daa17fd3ef6a0ccad8d9d79811d224d
Zebratic <48886580+Zebratic@users.noreply.github.com>
2021-06-09 08:05:17 +0200

    Add files via upload

---
+++ MousePos.Designer.cs
@@ -0,0 +1,174 @@
+﻿
+namespace MousePosForm
+{
+    partial class MousePos
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.MouseRecorder = new System.Windows.Forms.Timer(this.components);
+            this.XPos = new System.Windows.Forms.Label();
+            this.YPos = new System.Windows.Forms.Label();
+            this.MouseArea = new System.Windows.Forms.PictureBox();
+            this.Drawer = new System.Windows.Forms.Timer(this.components);
+            this.btnClear = new System.Windows.Forms.Button();
+            this.MaxDotsAllowed = new System.Windows.Forms.NumericUpDown();
+            this.lblMaxDots = new System.Windows.Forms.Label();
+            this.lblClickToDraw = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.MouseArea)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.MaxDotsAllowed)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // MouseRecorder
+            // 
+            this.MouseRecorder.Enabled = true;
+            this.MouseRecorder.Interval = 10;
+            this.MouseRecorder.Tick += new System.EventHandler(this.MouseRecorder_Tick);
+            // 
+            // XPos
+            // 
+            this.XPos.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.XPos.AutoSize = true;
+            this.XPos.Location = new System.Drawing.Point(407, 9);
+            this.XPos.Name = "XPos";
+            this.XPos.Size = new System.Drawing.Size(32, 13);
+            this.XPos.TabIndex = 0;
+            this.XPos.Text = "XPos";
+            // 
+            // YPos
+            // 
+            this.YPos.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.YPos.AutoSize = true;
+            this.YPos.Location = new System.Drawing.Point(407, 22);
+            this.YPos.Name = "YPos";
+            this.YPos.Size = new System.Drawing.Size(32, 13);
+            this.YPos.TabIndex = 1;
+            this.YPos.Text = "YPos";
+            // 
+            // MouseArea
+            // 
+            this.MouseArea.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.MouseArea.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.MouseArea.Location = new System.Drawing.Point(10, 9);
+            this.MouseArea.Name = "MouseArea";
+            this.MouseArea.Size = new System.Drawing.Size(391, 400);
+            this.MouseArea.TabIndex = 2;
+            this.MouseArea.TabStop = false;
+            // 
+            // Drawer
+            // 
+            this.Drawer.Enabled = true;
+            this.Drawer.Interval = 10;
+            this.Drawer.Tick += new System.EventHandler(this.Drawer_Tick);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnClear.Location = new System.Drawing.Point(407, 386);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(84, 23);
+            this.btnClear.TabIndex = 3;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // MaxDotsAllowed
+            // 
+            this.MaxDotsAllowed.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.MaxDotsAllowed.Location = new System.Drawing.Point(407, 360);
+            this.MaxDotsAllowed.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.MaxDotsAllowed.Name = "MaxDotsAllowed";
+            this.MaxDotsAllowed.Size = new System.Drawing.Size(84, 20);
+            this.MaxDotsAllowed.TabIndex = 4;
+            this.MaxDotsAllowed.Value = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            // 
+            // lblMaxDots
+            // 
+            this.lblMaxDots.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblMaxDots.AutoSize = true;
+            this.lblMaxDots.Location = new System.Drawing.Point(404, 344);
+            this.lblMaxDots.Name = "lblMaxDots";
+            this.lblMaxDots.Size = new System.Drawing.Size(95, 13);
+            this.lblMaxDots.TabIndex = 5;
+            this.lblMaxDots.Text = "Max Allowed Dots:";
+            // 
+            // lblClickToDraw
+            // 
+            this.lblClickToDraw.AutoSize = true;
+            this.lblClickToDraw.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.lblClickToDraw.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblClickToDraw.Location = new System.Drawing.Point(10, 9);
+            this.lblClickToDraw.Name = "lblClickToDraw";
+            this.lblClickToDraw.Size = new System.Drawing.Size(224, 37);
+            this.lblClickToDraw.TabIndex = 6;
+            this.lblClickToDraw.Text = "Click to draw!";
+            // 
+            // MousePos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(498, 421);
+            this.Controls.Add(this.lblClickToDraw);
+            this.Controls.Add(this.lblMaxDots);
+            this.Controls.Add(this.MaxDotsAllowed);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.MouseArea);
+            this.Controls.Add(this.YPos);
+            this.Controls.Add(this.XPos);
+            this.Name = "MousePos";
+            this.Text = "Mouse Pos";
+            ((System.ComponentModel.ISupportInitialize)(this.MouseArea)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.MaxDotsAllowed)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer MouseRecorder;
+        private System.Windows.Forms.Label XPos;
+        private System.Windows.Forms.Label YPos;
+        private System.Windows.Forms.PictureBox MouseArea;
+        private System.Windows.Forms.Timer Drawer;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.NumericUpDown MaxDotsAllowed;
+        private System.Windows.Forms.Label lblMaxDots;
+        private System.Windows.Forms.Label lblClickToDraw;
+    }
+}
+
---
+++ MousePos.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MousePosForm
+{
+    public partial class MousePos : Form
+    {
+        int MouseX = 0;
+        int MouseY = 0;
+        Panel XLine = new Panel();
+        Panel YLine = new Panel();
+        Panel MouseDot = new Panel();
+        List<Panel> DrawingDots = new List<Panel>();
+        Graphics DrawingArea;
+        bool HeldDown = false;
+        bool ReadyToReset = false;
+
+        public MousePos()
+        {
+            InitializeComponent();
+
+            #region Render properties
+            XLine.BackColor = Color.Red; // Set XLine color
+            YLine.BackColor = Color.Green; // Set YLine color
+            MouseDot.Size = new Size(12, 12); // Set dot Size
+            MouseDot.BackColor = Color.Blue; // Set dot color
+
+            this.Controls.Add(XLine); // Add the line to the form
+            this.Controls.Add(YLine); // Add the line to the form
+            this.Controls.Add(MouseDot); // Add the dot to the form
+            XLine.BringToFront(); // Put the line infront of the panel
+            YLine.BringToFront(); // Put the line infront of the panel
+            MouseDot.BringToFront(); // Put the dot infront of the panel
+            #endregion
+
+            this.DoubleBuffered = true; // Double buffered to increase performance and stop flickering
+        }
+
+        private void MouseRecorder_Tick(object sender, EventArgs e)
+        {
+            Point point = MouseArea.PointToClient(Cursor.Position); // Get mouse position and compare it to the panel's size
+            MouseX = point.X; // Save the X position for this current tick
+            MouseY = point.Y;// Save the Y position for this current tick
+
+            #region Calculate Mouse Position Inside Panel
+            if (MouseX <= 0) // if X Position is in negative numbers (its outside the panel) then set it to 0
+                MouseX = 0;
+            if (MouseY <= 0) // if Y Position is in negative numbers (its outside the panel) then set it to 0
+                MouseY = 0;
+
+            if (MouseX >= MouseArea.Size.Width) // if X position is greater than the panel's width, then set it to 0
+                MouseX = 0;
+            if (MouseY >= MouseArea.Size.Height) // if Y position is greater than the panel's height, then set it to 0
+                MouseY = 0;
+
+            if (MouseX == 0) // if X position is 0 (most likely been reset because it was outside the panel) then set the Y position to 0 aswell
+                MouseY = 0;
+
+            if (MouseY == 0) // if Y position is 0 (most likely been reset because it was outside the panel) then set the X position to 0 aswell
+                MouseX = 0;
+            #endregion
+
+            #region Update Line Lengths
+            XLine.Size = new Size(MouseArea.Width, 4); // set XLine Size
+            YLine.Size = new Size(4, MouseArea.Height); // set YLine Size
+            #endregion
+
+            #region Render Lines
+            if (MouseX != 0 && MouseY != 0) // Only render lines if both values are NOT 0 (aka mouse is inside the panel)
+            {
+                // remove the //'s if you want the lines to reappear when mouse is enters the panel again
+                //XLine.Visible = true;
+                //YLine.Visible = true;
+
+                XLine.BackColor = Color.Red; // Make the lines normal coloured again
+                YLine.BackColor = Color.Green; // Make the lines normal coloured again
+
+                XLine.Location = new Point(MouseArea.Location.X, MouseY + 7); // Set the lines position to where ever the mouse position is (7, because of alignment failures)
+                YLine.Location = new Point(MouseX + 7, MouseArea.Location.Y); // Set the lines position to where ever the mouse position is (7, because of alignment failures)
+            }
+            else
+            {
+                // remove the //'s if you want the lines to dissapear when mouse is outside the panel
+                //XLine.Visible = false;
+                //YLine.Visible = false;
+
+                XLine.BackColor = Color.DarkRed; // Darken the dot's color
+                YLine.BackColor = Color.DarkGreen; // Darken the dot's color
+            }
+            #endregion
+
+            #region Render Dot
+            if (MouseX != 0 && MouseY != 0) // Only render dot if both values are NOT 0 (aka mouse is inside the panel)
+            {
+                // remove the //'s if you want the dot to reappear when mouse is enters the panel again
+                //MouseDot.Visible = true;
+
+                MouseDot.BackColor = Color.Blue; // Make the lines normal coloured again
+
+                MouseDot.Location = new Point(MouseX + 3, MouseY + 3); // Set the dot's position to where ever the mouse position is (7, because of alignment failures)
+            }
+            else
+            {
+                // remove the //'s if you want the dot to dissapear when mouse is outside the panel
+                //MouseDot.Visible = false;
+
+                MouseDot.BackColor = Color.DarkBlue; // Darken the dot's color
+            }
+            #endregion
+
+            XPos.Text = MouseX.ToString(); // Display the Mouse's X position within the panel on a label
+            YPos.Text = MouseY.ToString(); // Display the Mouse's Y position within the panel on a label
+
+            DrawingArea = MouseArea.CreateGraphics(); // Update drawing area (if size change)
+        }
+
+        private void Drawer_Tick(object sender, EventArgs e)
+        {
+            if ((Control.MouseButtons & MouseButtons.Left) != 0 && MouseX != 0 && MouseY != 0) // Check if left mouse button is held down and if mouse is not outside the drawing area.
+            {
+                lblClickToDraw.Visible = false;
+                HeldDown = true;
+                ReadyToReset = true;
+                #region Add dot
+                Panel Dot = new Panel(); // Creates a new dot
+                Dot.Size = new Size(4, 4); // Sets the dot size to 4x4 pixels big
+                Dot.BackColor = Color.Cyan; // Sets the dot color to Cyan
+                Dot.Location = new Point(MouseX + 7, MouseY + 7); // Sets the dot color to Cyan
+                this.Controls.Add(Dot); // Add the new dot to the drawing area
+                Dot.BringToFront(); // Bring the new dot on top of everything else
+                DrawingDots.Add(Dot); // Add the new dot to the saved dot list
+                #endregion
+            }
+            else
+            {
+                HeldDown = false;
+            }
+
+            if (!HeldDown && ReadyToReset)
+            {
+                Panel SpaceDot = new Panel(); // Creates a new invisible dot
+                SpaceDot.Location = new Point(-69420, 0); // Sets the dot to the X position: -69420  (out of bounds)
+                DrawingDots.Add(SpaceDot); // Add the invisible dot to the saved dot list
+                ReadyToReset = false;
+            }
+
+            #region Connect to previous dot
+            foreach (Panel ListDot in DrawingDots)
+            {
+                try
+                {
+                    this.Invalidate(); // Redraws all the previous added connected lines
+                    Panel PreviousListDot = DrawingDots[DrawingDots.FindIndex(a => a == ListDot) - 1]; // Get the previous dot compared to the one in use
+                    if (PreviousListDot.Location.X != -69420 && ListDot.Location.X != -69420) // Check if its not a space
+                    {
+                        Point P1 = new Point(ListDot.Location.X - 9, ListDot.Location.Y - 9); // Get the exact point of the current dot
+                        Point P2 = new Point(PreviousListDot.Location.X - 9, PreviousListDot.Location.Y - 9); // Get the exact point of the previous dot
+                        DrawingArea.DrawLine(new Pen(Color.Black, 1), P1, P2); // Draw a line connecting the current and previous dot
+                    }
+                }
+                catch { }
+            }
+            #endregion
+
+            #region Remove last added dot
+            if (DrawingDots.Count >= MaxDotsAllowed.Value && MaxDotsAllowed.Value > 0)
+            {
+                this.Controls.Remove(DrawingDots[0]); // Remove the dot from the panel
+                DrawingDots.RemoveAt(0); // Remove the first added dot from the list
+                MouseArea.Refresh(); // Refreshes the drawing area to clean all the connected lines
+            }
+            #endregion
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            foreach (Panel dot in DrawingDots) // For every saved dot
+            {
+                this.Controls.Remove(dot); // Remove the dot from the panel
+            }
+            MouseArea.Refresh(); // Refreshes the drawing area to clean all the connected lines
+            DrawingDots.Clear(); // Clear all the saved dots
+        }
+    }
+}
